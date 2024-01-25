a6a9d51d6299b2f4726ef75be5edb352a84b9b7f
julio cesar <juliodantas19@gmail.com>
2024-01-25 00:11:45 -0300

    Responsivity

---
+++ src/app/components/forms/dist/forms.component.js
@@ -0,0 +1,38 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.FormsComponent = void 0;
+var core_1 = require("@angular/core");
+var FormsComponent = /** @class */ (function () {
+    function FormsComponent(formBuilder) {
+        this.formBuilder = formBuilder;
+        this.results = [];
+    }
+    FormsComponent.prototype.ngOnInit = function () {
+        this.formGroup = this.formBuilder.group({
+            hour: '',
+            showClosed: false
+        });
+    };
+    FormsComponent.prototype.onSubmit = function () {
+        console.log(this.formGroup.value);
+    };
+    FormsComponent.prototype.onClean = function () {
+        this.formGroup.reset();
+        console.log('Limpo!');
+    };
+    FormsComponent = __decorate([
+        core_1.Component({
+            selector: 'app-forms',
+            templateUrl: './forms.component.html',
+            styleUrls: ['./forms.component.scss']
+        })
+    ], FormsComponent);
+    return FormsComponent;
+}());
+exports.FormsComponent = FormsComponent;
